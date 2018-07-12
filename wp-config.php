3e0d4f3dd1404fd2f9a1f011049ab851fad84671
laks <laksparis@gmail.com>
2018-07-12 22:51:25 +0530

    changes

--- wp-config.php
+++ wp-config.php
@@ -26,7 +26,7 @@ define('DB_NAME', 'news');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'devroot');
 
 /** MySQL hostname */
 define('DB_HOST', '127.0.0.1');
